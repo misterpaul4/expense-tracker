4dd931c7d8f224287915de6405a794aa17aa0431
paul <ajuzepaul>
2020-08-11 01:25:35 +0300

    transactions can me modified and deleted

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -70,7 +70,7 @@ class TransactionsController < ApplicationController
   def destroy
     @transaction.destroy
     respond_to do |format|
-      format.html { redirect_to transactions_url, notice: 'Transaction was successfully destroyed.' }
+      format.html { redirect_to transactions_url, notice: 'Transaction was successfully deleted.' }
       format.json { head :no_content }
     end
   end
